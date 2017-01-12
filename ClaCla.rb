5723441a806c04b1beaccae47627d75b42763704
marineST <novalbatross0807@gmail.com>
2017-01-12 12:07:43 +0900

    added wizard unit to ClaCla

--- ClaCla.rb
+++ ClaCla.rb
@@ -18,5 +18,6 @@ barbarian = Unit.new(23,95,150,6,1)
 archer = Unit.new(20,40,300,8,1)
 giant = Unit.new(31,670,2250,40,5)
 dragon = Unit.new(180,2300,33000,240,30)
+wizard = Unit.new(170,156,3500,40,4)
 
 
